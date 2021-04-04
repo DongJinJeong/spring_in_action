b9026a9313e7feb2a213a1ba02bd5d11bc99714f
DongJinJeong <ehd5719@gmail.com>
2021-04-04 23:24:49 +0900

    JdbcTemplate

--- taco-cloud/src/main/java/tacos/Order.java
+++ taco-cloud/src/main/java/tacos/Order.java
@@ -1,9 +1,13 @@
 package tacos;
 
+import java.util.Date;
+
 import lombok.Data;
 
 @Data
 public class Order {
+	private Long id;
+	private Date placedAt;
 	private String deliveryName;
 	private String deliveryStreet;
 	private String deliveryCity;
--- taco-cloud/src/main/java/tacos/Taco.java
+++ taco-cloud/src/main/java/tacos/Taco.java
@@ -1,11 +1,14 @@
 package tacos;
 
+import java.util.Date;
 import java.util.List;
 
 import lombok.Data;
 
 @Data
 public class Taco {
+	private Long id;
+	private Date createAt;
 	private String name;
 	private List<String> ingredients;
 }
